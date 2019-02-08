6fde7bb9e61c47e3165c706706900938b0a77459
Goldoni <goldoni.fouotsa@desiretec.com>
2019-02-08 11:36:08 +0100

    ajust i18

--- Modules/TuiCameroon/Http/routes.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-Route::group(['domain' => 'tui cameroon'], function () {
-    Route::group(['middleware' => 'web', 'namespace' => 'Modules\TuiCameroon\Http\Controllers'], function () {
-        Route::get('/', 'TuiCameroonController@index');
-        Route::get('show', 'TuiCameroonController@show');
-        Route::post('store', 'TuiCameroonController@store')->name('store');
-    });
-});
