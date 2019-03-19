2de45935f739cb014455a6a8acca828fd8049dff
Ekrem Hajredini <ekrem@viable.is>
2019-03-19 14:11:49 +0100

    #6: seed for master language

--- Modules/Master/Database/Seeders/MasterLanguagesSeederTableSeeder.php
+++ Modules/Master/Database/Seeders/MasterLanguagesSeederTableSeeder.php
@@ -4,6 +4,7 @@ namespace Modules\Master\Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 
 class MasterLanguagesSeederTableSeeder extends Seeder
 {
@@ -15,5 +16,13 @@ class MasterLanguagesSeederTableSeeder extends Seeder
     public function run()
     {
         Model::unguard();
+
+        $language = DB::table('languages')->where('locale', \Config::get('master.locale'))->first();
+
+
+        DB::table('language_whitelabel')->insert([
+            'language_id' => $language->id,
+            'whitelabel_id' => \Config::get('master.id')
+        ]);
     }
 }
