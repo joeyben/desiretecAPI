e330fdab940d857a11a51609959dcf4d4002107b
Joe <joe@thisisleap.com>
2019-03-29 17:48:04 +0100

    trend tour local

--- Modules/Trendtours/Config/config.php
+++ Modules/Trendtours/Config/config.php
@@ -3,7 +3,6 @@
 return [
     'name' => 'Trendtours',
     'id'   => 39,
-    'email' => 'info@trendtours.de',
     'locale' => 'de',
     'language_lines_table' => 'language_lines_trendtours',
     'language_lines_model' => \Modules\LanguageLines\Entities\LanguageLines::class
