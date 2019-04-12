b80f02b8452a306b5d43a1df1e31afc4f3edbea4
Joe <joe@thisisleap.com>
2019-04-12 12:18:51 +0200

    Novasol fixes

--- Modules/Novasol/Http/routes.php
+++ Modules/Novasol/Http/routes.php
@@ -1,6 +1,6 @@
 <?php
 
-Route::group(['domain' => 'novasol.wunsch-service.de'], function () {
+Route::group(['domain' => 'novasol.org'], function () {
     setCurrentWhiteLabelId(\Config::get('novasol.id'));
     setTranslationLoaderModel(\Config::get('novasol.language_lines_model'));
     Route::group(['middleware' => 'web', 'namespace' => 'Modules\Novasol\Http\Controllers' , 'as' => 'novasol.'], function () {
