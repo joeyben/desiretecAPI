36501f99a47ce70129e7f219f5222c5ba3cf1cd4
Joe <joe@thisisleap.com>
2019-05-20 14:08:50 +0200

    new layer

--- Modules/Trendtours/Config/config.php
+++ Modules/Trendtours/Config/config.php
@@ -2,7 +2,7 @@
 
 return [
     'name' => 'Trendtours',
-    'id'   => 17,
+    'id'   => 39,
     'locale' => 'de',
     'url' => 'https://trendtours.'.\Config('APP_ENV_URL'),
     'language_lines_table' => 'language_lines_trendtours',
