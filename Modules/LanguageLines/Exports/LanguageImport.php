aedb0fe02212a0fc00c9af495374fb7f22a63acd
Votre Nom <Vous@exemple.com>
2019-07-02 10:20:55 +0200

    import

--- Modules/LanguageLines/Exports/LanguageImport.php
+++ Modules/LanguageLines/Exports/LanguageImport.php
@@ -4,6 +4,7 @@
 namespace Modules\LanguageLines\Exports;
 
 use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\DB;
 use Maatwebsite\Excel\Concerns\ToCollection;
 use Maatwebsite\Excel\Concerns\WithHeadingRow;
 use Modules\LanguageLines\Entities\LanguageLines;
@@ -21,13 +22,23 @@ class LanguageImport implements ToCollection, WithHeadingRow
     public function collection(Collection $rows)
     {
         foreach ($rows as $row) {
-            LanguageLines::create([
-                'locale' => $row['lokal'],
-                'group' => $row['gruppe'],
-                'description' => $row['beschreibung'],
-                'key' => $row['key'],
-                'text' => $row['text'],
-            ]);
+            $exist = DB::table('language_lines')
+                ->where('locale', $row['lokal'])
+                ->where('group', $row['gruppe'])
+                ->where('description', $row['beschreibung'])
+                ->where('key', $row['key'])
+                ->where('text', $row['text'])
+                ->exists();
+
+            if (!$exist) {
+                LanguageLines::create([
+                    'locale' => $row['lokal'],
+                    'group' => $row['gruppe'],
+                    'description' => $row['beschreibung'],
+                    'key' => $row['key'],
+                    'text' => $row['text'],
+                ]);
+            }
         }
     }
 }
