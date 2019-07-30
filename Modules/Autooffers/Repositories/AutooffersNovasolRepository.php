b6d226f282ca704a192dad654723fd189741895b
Mekki <mekki.alaoui@desiretec.com>
2019-07-30 17:21:34 +0200

    novasol api areas

--- Modules/Autooffers/Repositories/AutooffersNovasolRepository.php
+++ Modules/Autooffers/Repositories/AutooffersNovasolRepository.php
@@ -444,7 +444,7 @@ class AutooffersNovasolRepository extends BaseRepository
          $land = strtoupper($land);
          $code = DB::table('novasol_country')
             ->select('novasol_code')
-            ->where('UPPER(name)', '=', $land)
+            ->where('name', '=', $land)
             ->get()->first();
 
          if ($code === null){
@@ -452,11 +452,11 @@ class AutooffersNovasolRepository extends BaseRepository
              $code = DB::table('novasol_area')
                  ->join('novasol_country', 'novasol_area.novasol_country_id', '=', 'novasol_country.id')
                  ->select('novasol_code')
-                 ->where('UPPER(novasol_area.name)', '=', $land)
+                 ->where('novasol_area.name', '=', $land)
                  ->get()->first();
              $area = DB::table('novasol_area')
                  ->select('novasol_area_code')
-                 ->where('UPPER(name)', '=', $land)
+                 ->where('name', '=', $land)
                  ->get()->first();
              return [$code->novasol_code,$area->novasol_area_code];
          }else{
