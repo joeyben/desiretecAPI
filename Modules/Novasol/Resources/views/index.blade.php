f05195387107bac59c8cb4389a36b182a1d76051
Genc Gruda <genc.gruda@codefrog-it.de>
2019-08-05 17:18:30 +0200

    NOV-29 - Resize the big picture on different screens => implemented

--- Modules/Novasol/Resources/views/index.blade.php
+++ Modules/Novasol/Resources/views/index.blade.php
@@ -1,6 +1,18 @@
 @extends('novasol::layouts.master')
 
 @section('content')
+    <style type="text/css">
+        @media only screen and (min-width: 1400px) {
+            .slider{
+                height: 1110px;
+            }
+            .slider .layer-action{
+                top: 300px;
+            }
+        }
+    </style>
+
+
     <div class="slider" style="background-image: url({{ $bg_image }})">
         <div class="welcome">
             {{ trans('whitelabel.frontend.welcome') }}
@@ -8,7 +20,9 @@
         </div>
 
         <div class="layer-action">
-            <a href="javascript:showLayer();" class="btn btn-primary btn-md">{{ trans('whitelabel.frontend.create_wish') }}</a>
+            <a href="javascript:showLayer();" class="btn btn-primary btn-md">
+                {{ trans('whitelabel.frontend.create_wish') }}
+            </a>
         </div>
     </div>
 @endsection
