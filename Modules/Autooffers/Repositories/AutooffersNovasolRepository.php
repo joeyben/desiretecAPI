ee72a03a0146daa22a7e1b2dc9527a5247f8b3a6
Mekki <mekki.alaoui@desiretec.com>
2019-08-09 09:15:22 +0200

    debugging

--- Modules/Autooffers/Repositories/AutooffersNovasolRepository.php
+++ Modules/Autooffers/Repositories/AutooffersNovasolRepository.php
@@ -159,7 +159,6 @@ class AutooffersNovasolRepository extends BaseRepository
         $wish = Wish::find($wish_id);
 
         foreach ($properties as $key => $autooffer) {
-            die(var_dump($key));
             $offer = json_decode(json_encode($autooffer), true);
             $hotel = json_decode(json_encode($wish), true);
             //$hotel = json_decode(json_encode($this->getFullHotelData($offer['hotelOffer']['hotel']['giata']['hotelId'])), true);
