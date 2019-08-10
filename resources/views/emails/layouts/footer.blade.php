4c89343d0e55ce88be3f8a25cde7b8f68766b881
Genc Gruda <genc.gruda@codefrog-it.de>
2019-08-10 11:25:29 +0200

    NOV-18 - Setup send passwort email => extend the footer-layout

--- resources/views/emails/layouts/footer.blade.php
+++ resources/views/emails/layouts/footer.blade.php
@@ -1,10 +1,13 @@
 <p style="
             line-height: 24px;
             margin-top: 60px;
+            padding-top: 10px;
             padding-bottom: 20px;
             margin-bottom: 20px;
             border-top:1px solid #e7e7e7;
             border-bottom:1px solid #e7e7e7;
+            font-size: 12px;
+            color: #000;
             width:100%;">
    {!! trans('email.footer_novasol.line1') !!} <br>
     {!! trans('email.footer_novasol.line2') !!} <br><br>
