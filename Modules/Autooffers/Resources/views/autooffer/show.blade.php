2f8f2c95ee1f149092698a31292dea23a6ff32a0
Mekki <mekki.alaoui@desiretec.com>
2019-08-12 17:05:37 +0200

    add image slider

--- Modules/Autooffers/Resources/views/autooffer/show.blade.php
+++ Modules/Autooffers/Resources/views/autooffer/show.blade.php
@@ -193,7 +193,40 @@
                         <div class="card" id="hotel-0">
                             <div class="offer-content">
                                 <div class="offer-block offer-block--first">
-                                    <img src="{{ $offer->thumbnail }}" style="width: 100%">
+                                    <!-- Slideshow container -->
+                                    <div class="slideshow-container">
+
+                                        <!-- Full-width images with number and caption text -->
+                                        <div class="mySlides fade">
+                                            <div class="numbertext">1 / 3</div>
+                                            <img src="$images[0]" style="width:100%">
+                                            <div class="text">Caption Text</div>
+                                        </div>
+
+                                        <div class="mySlides fade">
+                                            <div class="numbertext">2 / 3</div>
+                                            <img src="$images[0]" style="width:100%">
+                                            <div class="text">Caption Two</div>
+                                        </div>
+
+                                        <div class="mySlides fade">
+                                            <div class="numbertext">3 / 3</div>
+                                            <img src="$images[0]" style="width:100%">
+                                            <div class="text">Caption Three</div>
+                                        </div>
+
+                                        <!-- Next and previous buttons -->
+                                        <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
+                                        <a class="next" onclick="plusSlides(1)">&#10095;</a>
+                                    </div>
+                                    <br>
+
+                                    <!-- The dots/circles -->
+                                    <div style="text-align:center">
+                                        <span class="dot" onclick="currentSlide(1)"></span>
+                                        <span class="dot" onclick="currentSlide(2)"></span>
+                                        <span class="dot" onclick="currentSlide(3)"></span>
+                                    </div>
                                 </div>
                                 <div class="offer-block no-border">
                                     <div class="stars hide-mobile">
@@ -548,5 +581,34 @@
           }, 2000);
         })
       });
+
+      var slideIndex = 1;
+      showSlides(slideIndex);
+
+      // Next/previous controls
+      function plusSlides(n) {
+        showSlides(slideIndex += n);
+      }
+
+      // Thumbnail image controls
+      function currentSlide(n) {
+        showSlides(slideIndex = n);
+      }
+
+      function showSlides(n) {
+        var i;
+        var slides = document.getElementsByClassName("mySlides");
+        var dots = document.getElementsByClassName("dot");
+        if (n > slides.length) {slideIndex = 1}
+        if (n < 1) {slideIndex = slides.length}
+        for (i = 0; i < slides.length; i++) {
+          slides[i].style.display = "none";
+        }
+        for (i = 0; i < dots.length; i++) {
+          dots[i].className = dots[i].className.replace(" active", "");
+        }
+        slides[slideIndex-1].style.display = "block";
+        dots[slideIndex-1].className += " active";
+      }
     </script>
 @endsection
