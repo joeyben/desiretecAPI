6c1c269485ba0310fc391b69f6056192a82f541e
Joe <joe@desiretec.com>
2019-09-05 14:44:37 +0000

    fix overland

---
+++ Modules/Overland/Config/config.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'name' => 'Overland',
+    'id'   => 94,
+    'locale' => 'de',
+    'language_lines_table' => 'language_lines_overland',
+    'language_lines_model' => \Modules\LanguageLines\Entities\LanguageLines::class,
+    'local_url' => 'overland.com',
+    'development_url' => 'overland.reise-wunsch.com',
+    'production_url' => 'overland.reisewunschservice.de',
+];
