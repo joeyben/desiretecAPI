a468ad04a5a5ca9beddca0fd472b51d00e233890
Mekki <mekki.alaoui@desiretec.com>
2019-09-05 14:20:16 +0200

    Travel Overland Whitelabel

---
+++ Modules/Overland/Config/config.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'name' => 'Overland',
+    'id'   => 89,
+    'locale' => 'de',
+    'language_lines_table' => 'language_lines_overland',
+    'language_lines_model' => \Modules\LanguageLines\Entities\LanguageLines::class,
+    'local_url' => 'traveloverland.com',
+    'development_url' => 'traveloverland.reise-wunsch.com',
+    'production_url' => 'traveloverland.reisewunschservice.de',
+];
