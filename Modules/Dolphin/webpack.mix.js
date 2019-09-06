e0e1f286ebf90a68d25d2ac318e355b326ac092a
Joe <joe@desiretec.com>
2019-09-06 06:39:16 +0000

    add dolphin

---
+++ Modules/Dolphin/webpack.mix.js
@@ -0,0 +1,45 @@
+const { mix } = require('laravel-mix');
+require('laravel-mix-merge-manifest');
+
+var base_url = '../../';
+
+mix.setPublicPath('../../public').mergeManifest();
+
+mix.options({
+    processCssUrls: false
+});
+
+mix.scripts(
+    [
+        base_url + "node_modules/jquery/dist/jquery.min.js",
+        base_url + '/resources/assets/js/layer/datepicker.js',
+        __dirname + '/Resources/assets/js/app.js'
+    ], __dirname + '/../../public/whitelabel/dolphin/js/dolphin.js')
+    .scripts([
+        base_url + '/resources/assets/js/layer/exitintent.js',
+        base_url + '/resources/assets/js/layer/exitintent-new.js',
+        base_url + '/node_modules/js-cookie/src/js.cookie.js',
+        base_url + '/resources/assets/js/layer/base.js',
+        base_url + '/resources/assets/js/layer/rangeslider.js',
+        base_url + '/resources/assets/js/layer/datepicker.js',
+        __dirname + '/Resources/assets/js/layer/layer.js',
+    ], __dirname + '/../../public/whitelabel/dolphin/js/layer/layer.js')
+    .scripts([
+        base_url + '/resources/assets/js/layer/exitintent.js',
+        base_url + '/node_modules/js-cookie/src/js.cookie.js',
+        base_url + '/resources/assets/js/layer/base.js',
+        base_url + '/resources/assets/js/layer/rangeslider.js',
+        __dirname + '/Resources/assets/js/layer/layer.js',
+    ], __dirname + '/../../public/whitelabel/dolphin/js/layer/layer-locale.js')
+    .sass(__dirname + '/Resources/assets/sass/layer/layer.scss', 'whitelabel/dolphin/css/layer/layer.css')
+    .sass(base_url + '/resources/assets/sass/layer/_datepicker.scss', 'whitelabel/dolphin/css/datepicker.css')
+    .sass(__dirname + '/Resources/assets/sass/app.scss', 'whitelabel/dolphin/css/dolphin.css')
+    .styles([
+        base_url + '/public/whitelabel/dolphin/css/datepicker.css',
+        base_url + '/public/whitelabel/dolphin/css/layer/layer.css',
+    ], __dirname + '/../../public/whitelabel/dolphin/css/layer/whitelabel.css')
+    .copy(__dirname +'/Resources/assets/images/', __dirname +'/../../public/whitelabel/dolphin/images/')
+    .copy(__dirname +'/Resources/assets/svg/', __dirname +'/../../public/whitelabel/dolphin/svg/');
+if (mix.inProduction()) {
+    mix.version();
+}
