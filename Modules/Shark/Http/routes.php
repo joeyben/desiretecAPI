5561269b1a070cb361dd3b9e54503e4b879413eb
Joe <joe@desiretec.com>
2019-09-07 14:52:02 +0000

    add shark

--- Modules/Shark/Http/routes.php
+++ Modules/Shark/Http/routes.php
@@ -10,14 +10,14 @@ $domain = $domain_env[\Config::get('app.js_env') . '_url'];
 Route::group(['domain' => $domain], function () {
     setCurrentWhiteLabelId(\Config::get('shark.id'));
     setTranslationLoaderModel(\Config::get('shark.language_lines_model'));
-    Route::group(['middleware' => 'web', 'namespace' => 'Modules\Dolphin\Http\Controllers', 'as' => 'shark.'], function () {
-        Route::get('/', 'DolphinController@index');
-        Route::get('show', 'DolphinController@show');
-        Route::get('store', 'DolphinController@store')->name('store');
-        Route::get('wish/{wish}', 'DolphinWishesController@view')->name('wish.view');
-        Route::get('wish/{wish}/{token}', 'DolphinWishesController@details')->name('wish.details');
-        Route::get('getwish/{wish}', 'DolphinWishesController@getWish')->name('getWish');
-        Route::get('wishlist/{token}', 'DolphinWishesController@validateTokenList');
+    Route::group(['middleware' => 'web', 'namespace' => 'Modules\Shark\Http\Controllers', 'as' => 'shark.'], function () {
+        Route::get('/', 'SharkController@index');
+        Route::get('show', 'SharkController@show');
+        Route::get('store', 'SharkController@store')->name('store');
+        Route::get('wish/{wish}', 'SharkWishesController@view')->name('wish.view');
+        Route::get('wish/{wish}/{token}', 'SharkWishesController@details')->name('wish.details');
+        Route::get('getwish/{wish}', 'SharkWishesController@getWish')->name('getWish');
+        Route::get('wishlist/{token}', 'SharkWishesController@validateTokenList');
     });
 });
 
