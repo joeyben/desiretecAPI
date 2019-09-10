a3aed7ce6d0b5a16cd502c1add50a48bf730173c
Joe <joe@desiretec.com>
2019-09-10 16:23:03 +0000

    fixes

--- Modules/Wishes/Resources/views/emails/wish.blade.php
+++ Modules/Wishes/Resources/views/emails/wish.blade.php
@@ -5,7 +5,6 @@
             <table border="0" width="100%" align="center" cellpadding="0" cellspacing="0" class="container590">
                 <tr>
                     <td align="left" style="color: #888888; width:20px; font-size: 16px; line-height: 24px;">
-                        <!-- section text ======-->
 
                         <p style="line-height: 24px; margin-bottom:15px;">
                             {{ trans('email.wish.created.user.header') }}
