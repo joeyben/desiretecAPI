c5bd96fbc2fdf467bb8333f2655c8da6b00d03df
Votre Nom <Vous@exemple.com>
2019-09-14 09:30:23 +0200

    ajust code

--- Modules/Autooffers/Http/Controllers/AutooffersNovasolController.php
+++ Modules/Autooffers/Http/Controllers/AutooffersNovasolController.php
@@ -2,6 +2,7 @@
 
 namespace Modules\Autooffers\Http\Controllers;
 
+use App\Models\OfferFiles\OfferFile;
 use App\Models\Wishes\Wish;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
@@ -11,6 +12,8 @@ use Illuminate\Support\Facades\Redirect;
 use Modules\Autooffers\Entities\Autooffer;
 use Modules\Autooffers\Http\Services\AutooffersNovasolService;
 use Modules\Autooffers\Repositories\AutooffersNovasolRepository;
+use Mpdf\Tag\Input;
+use Nwidart\Modules\Collection;
 
 class AutooffersNovasolController extends Controller
 {
@@ -62,7 +65,7 @@ class AutooffersNovasolController extends Controller
     public function __construct(AutooffersNovasolRepository $autooffers, AutooffersNovasolService $service)
     {
         $this->autooffers = $autooffers;
-        $this->service = $service;
+        $this->service    = $service;
     }
 
     /**
@@ -90,11 +93,10 @@ class AutooffersNovasolController extends Controller
      *
      * @return mixed
      */
-    public function create(Wish $wish)
-    {
+    public function create(Wish $wish){
         $offers = Autooffer::where('wish_id', $wish->id)->get();
-        if (0 === $offers->count()) {
-            //if(!isset($wish->id)){
+        if($offers->count() == 0){
+        //if(!isset($wish->id)){
             logger()->info('AufoofferNovasolController.php > create() wurde aufgerufen!');
             $this->autooffers->saveWishData($wish);
             $response = $this->autooffers->getNovasolData($this->service->prepareParamForNovasolApi($this->autooffers, $wish));
@@ -103,7 +105,7 @@ class AutooffersNovasolController extends Controller
             $this->autooffers->storeMany($response, $properties, $wish->id);
         }
         $offers = Autooffer::where('wish_id', $wish->id)->get();
-        foreach ($offers as $offer) {
+        foreach ($offers as $offer){
             $this->getProduct($offer->code);
         }
 
@@ -134,30 +136,28 @@ class AutooffersNovasolController extends Controller
      *
      * @return Response
      */
-    public function show(Wish $wish)
-    {
+    public function show(Wish $wish){
         $images = [];
         $capacities = [];
         $rooms = [];
         $sizes = [];
         $types = [];
         $autooffers = Autooffer::where('wish_id', $wish->id)->orderBy('totalPrice', 'asc')->paginate(5);
-        foreach ($autooffers as $autooffer) {
+        foreach ($autooffers as $autooffer){
             $images[] = DB::table('offer_files')->select('file')->where('offer_id', $autooffer->code)->get();
             $capacities[] = DB::table('offer_files')->select('capacity')->where('offer_id', $autooffer->code)->get();
             $rooms[] = DB::table('offer_files')->select('room')->where('offer_id', $autooffer->code)->get();
             $sizes[] = DB::table('offer_files')->select('size')->where('offer_id', $autooffer->code)->get();
             $types[] = DB::table('offer_files')->select('type')->where('offer_id', $autooffer->code)->get();
         }
-
         return view('autooffers::autooffer.show', [
             'autooffers' => $autooffers,
-            'wish'       => $wish,
-            'images'     => $images,
+            'wish' => $wish,
+            'images' => $images,
             'capacities' => $capacities,
-            'rooms'      => $rooms,
-            'sizes'      => $sizes,
-            'types'      => $types
+            'rooms' => $rooms,
+            'sizes' => $sizes,
+            'types' => $types
         ]);
     }
 
@@ -192,50 +192,52 @@ class AutooffersNovasolController extends Controller
     }
 
     /**
-     * Creates the url to novasol and opens it on new tab.
+     * Creates the url to novasol and opens it on new tab
      */
     public function toTheOffer($offer_id)
     {
-        $autooffer = Autooffer::find($offer_id);
 
-        if (null !== $autooffer) {
-            $novasol_area = DB::table('novasol_area')->select('*')->where('novasol_area_code', $autooffer->hotel_location_region_code)->first();
-            $novasol_country = DB::table('novasol_country')->select('*')->where('id', $novasol_area->novasol_country_id)->first();
-            $url = 'https://www.novasol.de/ferienhaeuser/';
-            $url .= str_replace(' ', '-', mb_strtolower($novasol_country->name)) . '/';
-            $url .= str_replace(' ', '-', mb_strtolower($novasol_area->name)) . '/';
-            $url .= str_replace(' ', '-', 'ferienwohnung-' . mb_strtolower($autooffer->hotel_location_name)) . '-' . mb_strtolower($autooffer->hotel_code) . '?';
-            $url .= 'agency=2051402&';
-            if ($autooffer->wish->adults > 0) {
-                $url .= 'adults=' . $autooffer->wish->adults . '&';
-            }
-            if ($autooffer->wish->kids > 0) {
-                $url .= 'children=' . $autooffer->wish->kids . '&';
-            }
-            if ($autooffer->wish->kids > 0) {
-                $url .= 'children=' . $autooffer->wish->kids . '&';
-            }
-            $url .= 'from=' . str_replace('-', '', $autooffer->wish->earliest_start) . '&';
-            $url .= 'to=' . str_replace('-', '', $autooffer->wish->latest_return);
+       $autooffer = Autooffer::find($offer_id);
+
+       if(!is_null($autooffer)){
+           $novasol_area    = DB::table('novasol_area')->select('*')->where('novasol_area_code', $autooffer->hotel_location_region_code)->first();
+           $novasol_country = DB::table('novasol_country')->select('*')->where('id', $novasol_area->novasol_country_id)->first();
+           $url = "https://www.novasol.de/ferienhaeuser/";
+           $url .= str_replace(' ', '-', strtolower($novasol_country->name)).'/';
+           $url .= str_replace(' ', '-', strtolower($novasol_area->name)).'/';
+           $url .= str_replace(' ', '-', 'ferienwohnung-'.strtolower($autooffer->hotel_location_name)).'-'.strtolower($autooffer->hotel_code).'?';
+           $url .= 'agency=2051402&';
+           if($autooffer->wish->adults > 0){
+               $url .= 'adults='.$autooffer->wish->adults.'&';
+           }
+           if($autooffer->wish->kids > 0){
+               $url .= 'children='.$autooffer->wish->kids.'&';
+           }
+           if($autooffer->wish->kids > 0){
+               $url .= 'children='.$autooffer->wish->kids.'&';
+           }
+           $url .= 'from='. str_replace('-', '', $autooffer->wish->earliest_start).'&';
+           $url .= 'to='. str_replace('-', '', $autooffer->wish->latest_return);
 
 //           https://www.novasol.de/ferienhaeuser/spanien/hinterland/ferienwohnung-córdoba-eac214?adults=2&children=0&from=20190812&to=20190819 => von uns erstellt
 //           https://www.novasol.de/ferienhaeuser/spanien/andalusien/ferienwohnung-cordoba-eac214?adults=2&children=3&from=20190812&to=20190819 => URL bei NOVASOL
 
-            //dd($url);
 
-            // todo: The NOVASOL-ID is mising into the URL
-            return Redirect::away($url);
-        }
+           //dd($url);
+
+           // todo: The NOVASOL-ID is mising into the URL
+           return Redirect::away($url);
+       }
     }
 
     public function getProduct($id)
     {
-        $url = 'https://safe.novasol.com/api/products/' . $id . '?salesmarket=208&season=2019';
+        $url = 'https://safe.novasol.com/api/products/'. $id . '?salesmarket=208&season=2019';
 
         $opts = [
-            'http' => [
-                'method' => 'GET',
-                'header' => "Accept-language: en\r\n" .
+            "http" => [
+                "method" => "GET",
+                "header" => "Accept-language: en\r\n" .
                     "Key: WEvoSrIfHvZtVhlyKIWYfP5WjGcPVB\r\n" .
                     "Host: novasol.reise-wunsch.com\r\n"
             ]
@@ -247,17 +249,17 @@ class AutooffersNovasolController extends Controller
         $file = file_get_contents($url, false, $context);
         $arr = [];
         $product = simplexml_load_string($file);
-        $offer_file = DB::table('offer_files')->select('file')->where('offer_id', $id)->get();
-        if (0 === $offer_file->count()) {
+        $offer_file =  DB::table('offer_files')->select('file')->where('offer_id', $id)->get();
+        if($offer_file->count() == 0) {
             foreach ($product->pictures as $picture) {
                 foreach ($picture->picture as $pic) {
                     $arr[] = [
                         'offer_id' => $id,
-                        'file'     => $pic->domain . $pic->path . $pic->file,
+                        'file' => $pic->domain . $pic->path . $pic->file,
                         'capacity' => $product->information->adultCount,
-                        'room'     => $product->buildings->building->room->count(),
-                        'size'     => $product->features->feature[0]->unit['size'],
-                        'type'     => $product->features->feature[0]->subgroup
+                        'room' => $product->buildings->building->room->count(),
+                        'size' => $product->features->feature[0]->unit['size'],
+                        'type' => $product->features->feature[0]->subgroup
                     ];
                 }
             }
