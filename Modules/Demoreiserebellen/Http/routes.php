d795c5965845e813d887276495f1f1a9afe8a69d
Joe <joe@desiretec.com>
2019-10-04 09:29:16 +0000

    Travelover

---
+++ Modules/Demoreiserebellen/Http/routes.php
@@ -0,0 +1,22 @@
+<?php
+$domain_env = array(
+    'local_url' => 'demoreiserebellen.com',
+    'development_url' => 'demoreiserebellen.reise-wunsch.com',
+    'production_url' => 'demoreiserebellen.reisewunschservice.de',
+);
+
+$domain = $domain_env[\Config::get('app.js_env') . '_url'];
+
+Route::group(['domain' => $domain], function () {
+    setCurrentWhiteLabelId(\Config::get('demoreiserebellen.id'));
+    setTranslationLoaderModel(\Config::get('demoreiserebellen.language_lines_model'));
+    Route::group(['middleware' => 'web', 'namespace' => 'Modules\Demoreiserebellen\Http\Controllers', 'as' => 'demoreiserebellen.'], function () {
+        Route::get('/', 'DemoreiserebellenController@index');
+        Route::get('show', 'DemoreiserebellenController@show');
+        Route::get('store', 'DemoreiserebellenController@store')->name('store');
+        Route::get('wish/{wish}/{token}', 'DemoreiserebellenWishesController@details')->name('wish.details');
+        Route::get('getwish/{wish}', 'DemoreiserebellenWishesController@getWish')->name('getWish');
+        Route::get('wishlist/{token}', 'DemoreiserebellenWishesController@validateTokenList');
+    });
+});
+
