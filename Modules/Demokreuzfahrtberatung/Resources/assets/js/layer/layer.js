fa06851d9383a4a6765a0bf280ae33525da5b815
Mekki <mekki.alaoui@desiretec.com>
2019-10-21 15:18:12 +0200

    Kreuzfahrtberatung letzte Änderungen

--- Modules/Demokreuzfahrtberatung/Resources/assets/js/layer/layer.js
+++ Modules/Demokreuzfahrtberatung/Resources/assets/js/layer/layer.js
@@ -57,7 +57,7 @@ var exitIntent = window.exitIntent || {};
             '<div class="kwp-logo"></div>' +
             '<div class="kwp-header-content">' +
             '<h1>' +
-            texts[variant].header + ' <br/>' +
+            texts[variant].header +
             '</h1>' +
             '<p>' +
             texts[variant].body +
