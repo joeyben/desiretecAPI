dd674c1dfa2c5ea0691ff4164f23bd55c159016c
Mekki <mekki.alaoui@desiretec.com>
2019-10-22 11:10:50 +0200

    drop down feature wie im Master auch für Reiserebellen übernehmen und font einheitlich

--- Modules/Demoreiserebellen/Resources/views/layer/created.blade.php
+++ Modules/Demoreiserebellen/Resources/views/layer/created.blade.php
@@ -3,7 +3,6 @@
     <p>
         Wir suchen jetzt nach passenden Angeboten für dich. In wenigen Minuten wirst du per E-Mail informiert und kannst dir deine individuellen Reiseangebote ansehen.
         <br><br>
-        Deine Reise-Experten der Reise-Rebellen GmbH<br>
-        Wenn Sie Ihren Reisewunsch noch einmal überprüfen oder ändern möchten, <a  href="{{ route('demoreiserebellen.wish.details', [$id, $token]) }}" target="_blank">klicken Sie bitte hier.</a>
+        Deine Reise-Experten der Reise-Rebellen GmbH
     </p>
 </div>
