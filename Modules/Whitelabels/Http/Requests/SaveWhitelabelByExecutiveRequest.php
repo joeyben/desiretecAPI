38481d59fda8dc2caa65bd758293dc1781ae6905
Votre Nom <Vous@exemple.com>
2019-11-11 12:31:48 +0100

    ajust routes

--- Modules/Whitelabels/Http/Requests/SaveWhitelabelByExecutiveRequest.php
+++ Modules/Whitelabels/Http/Requests/SaveWhitelabelByExecutiveRequest.php
@@ -16,8 +16,7 @@ class SaveWhitelabelByExecutiveRequest extends FormRequest
         return [
             'id'                 => 'required|int|min:1',
             'display_name'       => 'required|string|min:2|max:255',
-            'color'              => 'required|string|min:4|max:10gst
-            ',
+            'color'              => 'required|string|min:4|max:10',
             'sub_domain'         => 'required|regex:/^([a-zA-Z]+)(\s[a-zA-Z]+)*$/',
             'background'         => 'required|array|min:1',
             'logo'               => 'required|array|min:1',
