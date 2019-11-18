9149e0432dbf88fedeada7c1172ab2ea6cf37876
Pranav Pandhare <pranav.pandhare@desiretec.com>
2019-11-18 18:16:22 +0100

    website footer fix

--- Modules/Traveloverland/Resources/views/layouts/footer.blade.php
+++ Modules/Traveloverland/Resources/views/layouts/footer.blade.php
@@ -1,9 +1,9 @@
 <div class="footer">
     <div class="container">
         <div class="col-md-12">
-            <ul>
-                 @foreach (footers_by_whitelabel() as $footer)
-                     <li>
+            <ul class="list-inline">
+                @foreach (footers_by_whitelabel() as $footer)
+                     <li class="list-inline-item">
                         <a href="{{ $footer->url }}" target="_blank">{{ $footer->name }}</a>
                     </li>
                 @endforeach
