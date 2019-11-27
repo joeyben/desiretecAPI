d4253b26f640e8d2c0fcb6943dbbbb7b6fd1f850
Mekki <mekki.alaoui@desiretec.com>
2019-11-27 16:47:11 +0100

    WL setting: company logo and PDF Tab

--- Modules/Testkurenundwellness/Resources/views/layer/popup.blade.php
+++ Modules/Testkurenundwellness/Resources/views/layer/popup.blade.php
@@ -431,10 +431,15 @@
                   @endphp
                 @endif
                     {{ Form::checkbox('terms', null, key_exists('terms', $request) && $request['terms']  ? 'true' : null,['class' => $terms_class, 'required' => 'required']) }}
-                     <p>Ich habe die <a href="https://testkurenundwellness.reise-wunsch.com/pdfs/tnb_kurenundwellness.pdf" id="agb_link" target="_blank">Teilnahmebedingungen</a> und <a id="datenschutz" href="https://www.kurenundwellness.tv/datenschutzerklaerung/" target="_blank">Datenschutzrichtlinien</a> zur Kenntnis genommen und möchte meinen Reisewunsch absenden.</p>
+                     <p>Ich habe die <a href="/tnb" id="agb_link" target="_blank">Teilnahmebedingungen</a> und <a id="datenschutz" href="https://www.kurenundwellness.tv/datenschutzerklaerung/" target="_blank">Datenschutzrichtlinien</a> zur Kenntnis genommen und möchte meinen Reisewunsch absenden.</p>
                 </div>
             </div>
         </div>
     </div>
 </div>
+<style>
+    .kwp-logo {
+        background: transparent url({{ getWhiteLabelLogoUrl() }}) no-repeat left top;
+    }
+</style>
 {{ Form::close() }}
