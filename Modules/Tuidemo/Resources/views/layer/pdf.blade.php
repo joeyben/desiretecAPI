499cbe8c655e56e058cbf33d4183c4afa2aa543d
Mekki <mekki.alaoui@desiretec.com>
2019-11-27 12:31:10 +0100

    Tab PDF immer Kundenname nicht desiretec MVP

---
+++ Modules/Tuidemo/Resources/views/layer/pdf.blade.php
@@ -0,0 +1,10 @@
+@extends('frontend.layouts.app')
+@section('before-scripts')
+    <style>
+        body, html {height: 100%;}
+        #app{display: none}
+    </style>
+    <object data="https://tui.reise-wunsch.com/pdfs/tnb_tui.pdf" type="application/pdf" style="height: 100%;width: 100%">
+        <embed src="https://tui.reise-wunsch.com/pdfs/tnb_tui.pdf" type="application/pdf" />
+    </object>
+@endsection
