441014ac8f0cba35e85b68530789114f4eeb1418
Joe <joe@desiretec.com>
2019-12-09 11:03:38 +0000

    merge with dev

--- Modules/Autooffers/Resources/views/autooffer/list_tt.blade.php
+++ Modules/Autooffers/Resources/views/autooffer/list_tt.blade.php
@@ -207,7 +207,7 @@
                                     </ul>
 
                                     <div class="travel-info">
-                                        <h4 data-toggle="tooltip" data-placement="bottom" title="{{ $offer['data']['offerFeatures'] }}">{{ $offer['data']['duration'] }} Tage, {{ str_limit($offer['data']['offerFeatures'], 20, "...") }}</h4>
+                                        <h4 data-toggle="tooltip" data-placement="bottom" title="{{ $offer['data']['offerFeatures'] }}">{{ $offer['data']['duration'] }} {{ trans('autooffers.offer.nights') }}, {{ str_limit($offer['data']['offerFeatures'], 20, "...") }}</h4>
                                         <h4>{{ trans('hotel.offer.boardtype.'.strtolower($offer['data']['boardType'])) }}</h4>
                                     </div>
                                 </div>
