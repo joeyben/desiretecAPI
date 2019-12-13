21283670d1c1f2e6863951c2eb742bb10cf4284d
Goldoni <fotsa.goldoni@yahoo.fr>
2019-12-13 13:18:20 +0100

    ajust redirect after reset password

--- app/Helpers/helpers.php
+++ app/Helpers/helpers.php
@@ -29,7 +29,7 @@ if (!function_exists('homeRoute')) {
         if (access()->allow('view-backend')) {
             return 'admin.dashboard';
         } elseif (auth()->check()) {
-            return 'frontend.user.dashboard';
+            return 'frontend.index';
         }
 
         return 'frontend.index';
