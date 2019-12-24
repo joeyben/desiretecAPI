bbd7589774e7f9fe88f3d9817823e6d48d4797f1
Joe <joe@desiretec.com>
2019-12-24 10:12:46 +0000

    revert miss deleted files

---
+++ Modules/Demoreiserebellen/Database/Migrations/2019_10_04_112718_create_language_lines_demoreiserebellen_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLanguageLinesDemoreiserebellenTable extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up()
+    {
+        if (!Schema::hasTable('language_lines_demoreiserebellen')) {
+            Schema::create('language_lines_demoreiserebellen', function (Blueprint $table) {
+                $table->increments('id');
+                $table->string('locale');
+                $table->string('description')->nullable();
+                $table->string('group');
+                $table->index('group');
+                $table->string('key');
+                $table->text('text');
+                $table->timestamps();
+            });
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down()
+    {
+        if (Schema::hasTable('language_lines_demoreiserebellen')) {
+            Schema::drop('language_lines_demoreiserebellen');
+        }
+    }
+}
