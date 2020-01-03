046eab1d046560ff8187a9734a77bfedbf8b6770
Pranav Pandhare <pranav.pandhare@desiretec.com>
2020-01-03 16:59:36 +0100

    mousehover

--- resources/views/frontend/wishes/partial/wish-user-details.blade.php
+++ resources/views/frontend/wishes/partial/wish-user-details.blade.php
@@ -22,7 +22,11 @@
 
     <div class="col-md-3">
         <i class="fal fa-plane-departure"></i>
-        <input class="data-content" value="{{ $wish->airport }}">
+        <input class="data-content airport-mousehover" value="{{ $wish->airport }}">
+        <span id="departure-mousehover"></span>
+        <div class="departure-tooltip">
+            {{ $wish->airport }}
+        </div>
     </div>
     <div class="col-md-3">
         <i class="fal fa-calendar-alt"></i>
@@ -39,7 +43,11 @@
 
     <div class="col-md-3">
         <i class="fal fa-plane-arrival"></i>
-        <input class="data-content" value="{{ $wish->destination }}">
+        <input class="data-content airport-mousehover" value="{{ $wish->destination }}">
+        <span id="arrival-mousehover"></span>
+        <div class="arrival-tooltip">
+            {{ $wish->destination }}
+        </div>
     </div>
     <div class="col-md-3">
         <i class="fal fa-users"></i>
@@ -93,3 +101,11 @@
     @endforeach
 </div>
 @endif
+
+@section('after-scripts')
+    <script type="application/javascript">
+        $(document).ready(function() {
+            alert("document ready occurred!");
+        });
+    </script>
+@endsection
