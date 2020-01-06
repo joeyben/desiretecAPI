9dd83be3329e95459c868e5cde0016ca08f888a6
Joe <joe@desiretec.com>
2020-01-06 17:23:19 +0000

    fix email contact user

--- Modules/Tui/Resources/assets/js/layer/layer.js
+++ Modules/Tui/Resources/assets/js/layer/layer.js
@@ -1662,4 +1662,16 @@ var exitIntent = window.exitIntent || {};
                     this.rows = minRows + rows;
                 });
         }
+
+        function parseDate(str) {
+            var mdy = str.split('.');
+            return new Date(mdy[2], mdy[1]-1, mdy[0]);
+        }
+
+        function datediff(first, second) {
+            // Take the difference between the dates and divide by milliseconds per day.
+            // Round to nearest whole number to deal with DST.
+            return Math.round((second-first)/(1000*60*60*24));
+        }
+
     })(jQuery);
