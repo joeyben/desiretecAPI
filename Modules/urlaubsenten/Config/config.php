5048c7f2f0eb55c1f39d2b95eadc5879cabf12ff
Milena Petkanova <milena.petkanova@motion-software.com>
2020-01-07 15:04:55 +0200

    create new wl

---
+++ Modules/urlaubsenten/Config/config.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'name' => 'Urlaubsenten',
+    'id'   => 203,
+    'locale' => 'de',
+    'language_lines_table' => 'language_lines_urlaubsenten',
+    'language_lines_model' => \Modules\LanguageLines\Entities\LanguageLines::class,
+    'local_url' => 'urlaubsenten.com',
+    'development_url' => 'urlaubsenten.reise-wunsch.com',
+    'production_url' => 'urlaubsenten.reisewunschservice.de',
+];
