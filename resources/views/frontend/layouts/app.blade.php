622239478384eae131f30e97221286c29cbf2ba2
Goldoni <fotsa.goldoni@yahoo.fr>
2020-01-08 13:03:40 +0100

    ajust success message

--- resources/views/frontend/layouts/app.blade.php
+++ resources/views/frontend/layouts/app.blade.php
@@ -61,7 +61,7 @@
             @include('frontend.includes.nav')
 
             <div class="container main-container">
-                @include('includes.alert')
+                @include('includes.partials.messages')
                 <div class="container">
                     <div class="row">
                         <div class="col-md-8 col-md-offset-2">
