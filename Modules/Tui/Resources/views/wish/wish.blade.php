e94e5fa5a7353ea92edb19a609437ced723f5297
Mekki <mekki.alaoui@desiretec.com>
2020-01-17 15:37:04 +0100

    email mistake in seller view

--- Modules/Tui/Resources/views/wish/wish.blade.php
+++ Modules/Tui/Resources/views/wish/wish.blade.php
@@ -486,7 +486,7 @@
                             </div>
                             <div class="mc-mail">
                                 <span class="glyphicon glyphicon-envelope"></span>
-                                <a href="mailto:mail@reisebuero.de">@if(count($wish->group->users[0]->agents)){{ $wish->group->users[0]->agents[0]->email }}@endif</a>
+                                <a href="mailto:@if(count($wish->group->users[0]->agents)){{ $wish->group->users[0]->agents[0]->email }}@endif">@if(count($wish->group->users[0]->agents)){{ $wish->group->users[0]->agents[0]->email }}@endif</a>
                             </div>
                         </div>
                     </div>
@@ -573,7 +573,7 @@
                             </div>
                             <div class="mc-mail">
                                 <span class="glyphicon glyphicon-envelope"></span>
-                                <a href="mailto:mail@reisebuero.de">@if(count($wish->group->users[0]->agents)){{ $wish->group->users[0]->agents[0]->email }}@endif</a>
+                                <a href="mailto:@if(count($wish->group->users[0]->agents)){{ $wish->group->users[0]->agents[0]->email }}@endif">@if(count($wish->group->users[0]->agents)){{ $wish->group->users[0]->agents[0]->email }}@endif</a>
                             </div>
                         </div>
                     </div>
