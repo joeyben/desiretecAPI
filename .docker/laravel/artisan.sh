9959b748418386bc5a93d6423eddd3fb1e069d54
Aljoscha Dembowsky <aljoscha@dembowsky.io>
2020-01-21 22:49:17 +0100

    Final CI Update *fingers crossed*
    - frontend container is sharing mix-manifest.json with the app container
    - updated entrypoints to take care of the linking etc.

--- .docker/laravel/artisan.sh
+++ .docker/laravel/artisan.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if [ -f /var/www/html/storage/mix-manifest.json ]; then
+    touch /var/www/html/storage/mix-manifest.json
+fi
+ln -s /var/www/html/storage/mix-manifest.json /var/www/html/public/mix-manifest.json
+
 php /var/www/html/artisan storage:link
 php /var/www/html/artisan migrate --force
 
@@ -7,7 +12,7 @@ php /var/www/html/artisan cache:clear
 php /var/www/html/artisan config:clear
 
 php /var/www/html/artisan config:cache
-php /var/www/html/artisan view:cache
+#php /var/www/html/artisan view:cache
 
 
 exit 0
