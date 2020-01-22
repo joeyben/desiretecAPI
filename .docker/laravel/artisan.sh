a6d0e7a717bf6b5fe57d36503621b611ff63b016
Aljoscha Dembowsky <aljoscha@dembowsky.io>
2020-01-22 09:37:26 +0100

    added trustProxies middleware again to make https work

--- .docker/laravel/artisan.sh
+++ .docker/laravel/artisan.sh
@@ -5,7 +5,6 @@ if [ -f /var/www/html/storage/mix-manifest.json ]; then
 fi
 ln -s /var/www/html/storage/mix-manifest.json /var/www/html/public/mix-manifest.json
 
-php /var/www/html/artisan storage:link
 php /var/www/html/artisan migrate --force
 
 php /var/www/html/artisan cache:clear
