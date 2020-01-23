1315d5fd1cf252f5f4fefaf35dfbd3bfc1cd1ce9
Aljoscha Dembowsky <aljoscha@dembowsky.io>
2020-01-23 09:07:04 +0100

    Updated artisan commands using su

--- .docker/laravel/artisan.sh
+++ .docker/laravel/artisan.sh
@@ -5,19 +5,21 @@ if [ -f /var/www/html/storage/mix-manifest.json ]; then
 fi
 ln -s /var/www/html/storage/mix-manifest.json /var/www/html/public/mix-manifest.json
 
-php /var/www/html/artisan migrate --force
+#ä Using su to avoid that the logfile of laravel is created as root when an fatal error happens while executing those commands
 
-php /var/www/html/artisan cache:clear
-php /var/www/html/artisan config:clear
+su -s /bin/bash www-data -c "php /var/www/html/artisan migrate --force"
 
-php /var/www/html/artisan config:cache
+su -s /bin/bash www-data -c "php /var/www/html/artisan cache:clear"
+su -s /bin/bash www-data -c "php /var/www/html/artisan config:clear"
+
+su -s /bin/bash www-data -c "php /var/www/html/artisan config:cache"
 #php /var/www/html/artisan view:cache
 
 # create symbolic link from storage/app/public to public/storage!
-php /var/www/html/artisan storage:link
-php /var/www/html/artisan laroute:generate -p public/storage/js
-php /var/www/html/artisan lang:js --no-lib resources/assets/js/utils/messages.js
-php /var/www/html/artisan lang:js public/storage/js/messages.js
+su -s /bin/bash www-data -c "php /var/www/html/artisan storage:link"
+su -s /bin/bash www-data -c "php /var/www/html/artisan laroute:generate -p public/storage/js"
+su -s /bin/bash www-data -c "php /var/www/html/artisan lang:js --no-lib resources/assets/js/utils/messages.js"
+su -s /bin/bash www-data -c "php /var/www/html/artisan lang:js public/storage/js/messages.js"
 
 
 exit 0
