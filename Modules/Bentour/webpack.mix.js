86f753fc88264f2c53b1c0a4b12d1de8da8ef8e0
Milena Petkanova <milena.petkanova@motion-software.com>
2020-01-24 13:02:45 +0200

    add more js resources on webpack

--- Modules/Bentour/webpack.mix.js
+++ Modules/Bentour/webpack.mix.js
@@ -24,6 +24,9 @@ mix.scripts(
         base_url + '/resources/assets/js/layer/datepicker.js',
         base_url + '/resources/assets/js/layer/devicedetector.min.js',
         base_url + '/resources/assets/js/layer/touchswipe.js',
+        base_url + '/resources/assets/js/layer/typeahead.js',
+        base_url + '/resources/assets/js/layer/bootstrap3-typeahead.min.js',
+        base_url + '/resources/assets/js/layer/tagsinput.min.js',
         __dirname + '/Resources/assets/js/layer/layer.js',
     ], __dirname + '/../../public/whitelabel/bentour/js/layer/layer.js')
     .scripts([
