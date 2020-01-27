184b4ee1b861b1f12b24e78f82c5cb4ed6f82d24
Joe <joe@desiretec.com>
2020-01-27 16:24:46 +0000

    new stuff

--- Modules/Autooffers/Repositories/AutooffersRepository.php
+++ Modules/Autooffers/Repositories/AutooffersRepository.php
@@ -282,6 +282,7 @@ class AutooffersRepository extends BaseRepository
                     'data'       => json_decode($offer['data'], true),
                     'hotel_data' => json_decode($offer['hotel_data'], true),
                     'personPrice'=> $offer['personPrice'],
+                    'status' => $offer['status']
                 ]
             );
         }
