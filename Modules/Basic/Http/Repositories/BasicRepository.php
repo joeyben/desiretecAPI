09cdb7ddd91360a580afdb1f06b1037eee9d0c30
Genc Gruda <genc.gruda@codefrog-it.de>
2020-02-03 17:10:51 +0100

    merge dev

---
+++ Modules/Basic/Http/Repositories/BasicRepository.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: gencgruda
+ * Date: 2019-12-23
+ * Time: 13:41
+ */
+
+namespace Modules\Basic\Http\Repositories;
+
+
+use http\Env\Request;
+use Modules\Basic\Http\Services\BasicService;
+
+class BasicRepository
+{
+    protected $basicService;
+
+    /**
+     * @param \Modules\Autooffers\Repositories\AutooffersRepository $autooffers
+     */
+    public function __construct(BasicService $service)
+    {
+        $this->basicService = $service;
+    }
+
+    public function getLayersData(Request $request, $whitelabel){
+        $layerData = [];
+        foreach ($whitelabel['whitelable_layer'] as $whitelableLayer) {
+            $layer['name'] = $whitelableLayer['layer']['category'] ;
+            $layer['active'] = $this->basicService->isLayerActive($request, $whitelableLayer);
+            array_push($layerData, $layer);
+        }
+      //  dd($layerData);
+        // todo #2: loop the data and call the service-function isLayerActive() and add the param "active"
+    }
+}
---
+++ Modules/Basic/Http/Services/BasicService.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: gencgruda
+ * Date: 2019-12-23
+ * Time: 13:29
+ */
+
+namespace Modules\Basic\Http\Services;
+
+
+use App\Http\Requests\Request;
+
+class BasicService{
+
+    /**
+     * @param Request $request
+     * @return bool
+     */
+    public function isLayerActive(Request $request, $whitelableLayer) {
+        // todo: check the db-url and the request-url
+
+
+
+//        dd($whitelableLayer, 'asd');
+//        if(){
+//
+//        }
+        return true;
+    }
+
+}
