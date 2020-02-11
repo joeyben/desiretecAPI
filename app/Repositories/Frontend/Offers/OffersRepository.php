61536bfc34497bfa677d8ab7b38815892f874b0e
Pranav Pandhare <pranav.pandhare@desiretec.com>
2020-02-11 11:27:03 +0100

    agents problem

--- app/Repositories/Frontend/Offers/OffersRepository.php
+++ app/Repositories/Frontend/Offers/OffersRepository.php
@@ -151,30 +151,10 @@ class OffersRepository extends BaseRepository
         $input = $request->except('_token', 'file');
         DB::transaction(function () use ($input, $files) {
             $id = access()->user()->id;
+            $active_agent = Agent::where('user_id', $id)->where('status', 'Active')->first();
 
             $input['created_by'] = $id;
-            $input['agent_id'] = Auth::guard('agent')->user()->id;
-
-            if ($offer = Offer::create($input)) {
-                $fileUploaded = $this->uploadImage($files, $offer->id);
-                event(new OfferCreated($offer));
-
-                return true;
-            }
-
-            throw new GeneralException(trans('exceptions.backend.offers.create_error'));
-        });
-    }
-
-    public function createTemp(Request $request)
-    {
-        $files = $request->hasfile('file') ? $request->file('file') : [];
-        $input = $request->except('_token', 'file');
-        DB::transaction(function () use ($input, $files) {
-            $id = Auth::guard('api')->user()->id;
-
-            $input['created_by'] = $id;
-            $input['agent_id'] = 9 /*Auth::guard('agent')->user()->id*/;
+            $input['agent_id'] = $active_agent['id'];
 
             if ($offer = Offer::create($input)) {
                 $fileUploaded = $this->uploadImage($files, $offer->id);
