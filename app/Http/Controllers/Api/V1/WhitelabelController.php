923071e642af7be93dfc0cc86941653a568c071d
Goldoni <fotsa.goldoni@yahoo.fr>
2020-02-11 16:32:26 +0100

    ajust module

--- app/Http/Controllers/Api/V1/WhitelabelController.php
+++ app/Http/Controllers/Api/V1/WhitelabelController.php
@@ -36,6 +36,8 @@ class WhitelabelController extends Controller
             new Where('name', $slug),
         ])->first();
 
+        $result['data'] = $whitelabel;
+
         $background = $this->moduleWhitelabelsRepository->getBackgroundImage($whitelabel);
 
         $logo = $this->moduleWhitelabelsRepository->getLogo($whitelabel);
@@ -44,11 +46,10 @@ class WhitelabelController extends Controller
 
         $visual = $this->moduleWhitelabelsRepository->getVisual($whitelabel);
 
-        $result['data'] = $whitelabel;
-        $result['data']['background'] = (null !== $background && null !== $background->first()) ? $background->first()['url'] : 'https://desiretec.s3.eu-central-1.amazonaws.com/uploads/whitelabels/background/1581433096bg.jpg';
-        $result['data']['logo'] = (null !== $logo && null !== $logo->first()) ? $logo->first()['url'] : 'https://desiretec.s3.eu-central-1.amazonaws.com/uploads/whitelabels/logo/1581433101snapchat_PNG65.png';
-        $result['data']['favicon'] = (null !== $favicon && null !== $favicon->first()) ? $favicon->first()['url'] : 'https://desiretec.s3.eu-central-1.amazonaws.com/uploads/whitelabels/logo/1581433101snapchat_PNG65.png';
-        $result['data']['visual'] = (null !== $visual && null !== $visual->first()) ? [$visual->first()] : [];
+        $result['data']['attachments']['background'] = (null !== $background && null !== $background->first()) ? $background->first()['url'] : 'https://desiretec.s3.eu-central-1.amazonaws.com/uploads/whitelabels/background/1581433096bg.jpg';
+        $result['data']['attachments']['logo'] = (null !== $logo && null !== $logo->first()) ? $logo->first()['url'] : 'https://desiretec.s3.eu-central-1.amazonaws.com/uploads/whitelabels/logo/1581433101snapchat_PNG65.png';
+        $result['data']['attachments']['favicon'] = (null !== $favicon && null !== $favicon->first()) ? $favicon->first()['url'] : 'https://desiretec.s3.eu-central-1.amazonaws.com/uploads/whitelabels/logo/1581433101snapchat_PNG65.png';
+        $result['data']['attachments']['visual'] = (null !== $visual && null !== $visual->first()) ? $visual->first()['url'] : '';
         $result['data']['layers'] = $whitelabel->layers;
 
         return $this->responseJson($result);
