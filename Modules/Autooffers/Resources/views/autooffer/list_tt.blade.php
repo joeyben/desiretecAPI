731bd5212646442ba8a5f05aab0cc2aae357e079
Joe <joe@thisisleap.com>
2020-02-12 13:16:35 +0100

    auto merfe

--- Modules/Autooffers/Resources/views/autooffer/list_tt.blade.php
+++ Modules/Autooffers/Resources/views/autooffer/list_tt.blade.php
@@ -165,12 +165,12 @@
                     <ul class="offers">
 
                         @foreach($offers as $key => $offer)
-                            @if (!$offer['hotel_data']['hotel'])
+                            @if (!$offer['hotel_data']['data'])
                                 @continue
                             @endif
                             @php
                                 $hotelData = [
-                                    'title' => str_replace("'","",$offer['hotel_data']['hotel']['name']),
+                                    'title' => str_replace("'","",$offer['hotel_data']['data']['name']),
                                     'stars' => key_exists('Hotelkategorie', $offer['hotel_data']['data']) ? intval($offer['hotel_data']['data']['Hotelkategorie']) : 0 ,
                                     'text' => $offer['data']['boardType'],
                                     'longitude' => $offer['data']['hotel_geo']['longitude'],
