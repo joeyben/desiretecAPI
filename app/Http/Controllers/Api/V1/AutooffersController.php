3a1bb599fc0fe44d38c3d82867544b522a508e48
Milena Petkanova <milena.petkanova@motion-software.com>
2020-02-18 16:35:00 +0200

    add more data to autooffers response

--- app/Http/Controllers/Api/V1/AutooffersController.php
+++ app/Http/Controllers/Api/V1/AutooffersController.php
@@ -7,25 +7,39 @@ use App\Repositories\Frontend\Wishes\WishesRepository;
 use Modules\Autooffers\Repositories\AutooffersRepository;
 use Modules\Autooffers\Repositories\AutooffersTTRepository;
 use Modules\Autooffers\Repositories\Eloquent\EloquentAutooffersRepository;
+use App\Models\KeywordList;
 
 class AutooffersController extends APIController implements AutooffersControllerInterface
 {
     private $autooffers;
     private $TTautooffers;
     private $rules;
+    private $keywordList;
 
-    public function __construct(WishesRepository $wish, AutooffersRepository $autooffers, AutooffersTTRepository $TTautooffers, EloquentAutooffersRepository $rules)
+    public function __construct(WishesRepository $wish, AutooffersRepository $autooffers, AutooffersTTRepository $TTautooffers, EloquentAutooffersRepository $rules, KeywordList $keywordList)
     {
         $this->wish = $wish;
         $this->autooffers = $autooffers;
         $this->TTautooffers = $TTautooffers;
         $this->rules = $rules;
+        $this->keywordList = $keywordList;
     }
 
     public function list(int $wishId)
     {
         try {
             $offers['data'] = $this->autooffers->getOffersDataFromId($wishId);
+
+            foreach ($offers as $offer) {
+
+                for ($i = 0; $i < 3; $i++) {
+                    $keyword = $offer['data']['hotelOffer']['hotel']['keywordList'][$i];
+                    $keywordCode = $this->keywordList::where('code', $keyword)->first();
+                    $keywordName = $keywords ? $keywords->name : '';
+
+                    $offer['data']['hotelOffer']['hotel']['keywordHighlights'][$i] = $keywordName;
+                }
+            }
 
             return $this->responseJson($offers);
         } catch (Exception $e) {
@@ -37,6 +51,33 @@ class AutooffersController extends APIController implements AutooffersController
     {
         try {
             $offers['data'] = $this->autooffers->getOffersDataFromId($wishId);
+
+            foreach ($offers as $offer) {
+
+                for ($i = 0; $i < 3; $i++) {
+                    $keywordCode = $offer['data']['hotelOffer']['hotel']['keywordList'][$i];
+                    $keyword = $this->keywordList::where('code', $keywordCode)->first();
+                    $keywordName = $keywords ? $keywords->name : '';
+
+                    $offer['data']['hotelOffer']['hotel']['keywordHighlights'][$i] = $keywordName;
+                }
+            }
+
+            return $this->responseJson($offers);
+        } catch (Exception $e) {
+            return $this->respondWithError($e);
+        }
+    }
+
+    public function getKeywords($offers)
+    {
+        try {
+            for ($i = 0; $i < 3; $i++) {
+                $offer['data']['hotelOffer']['hotel']['keywordList'][$i];
+            }
+
+            $keywords = $this->keywordList::where('code', $value)->first();
+            dd($keywords);
 
             return $this->responseJson($offers);
         } catch (Exception $e) {
