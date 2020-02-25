b7a13f5556a57766e5010448b224d4676c67be75
Goldoni <fotsa.goldoni@yahoo.fr>
2020-02-25 13:03:11 +0100

    ajust menu

--- app/Services/Flag/Src/Flag.php
+++ app/Services/Flag/Src/Flag.php
@@ -80,12 +80,12 @@ class Flag implements FlagInterface
                 'url'   => route('provider.whitelabels')
             ],
             2 => [
-                'name'  => 'Step 2: Layer version',
+                'name'  => 'Step 2: Layer Version',
                 'route' => 'admin.whitelabels.layers',
                 'url'   => route('admin.whitelabels.layers')
             ],
             3 => [
-                'name'  => 'Step 3: Layer content',
+                'name'  => 'Step 3: Layer Content',
                 'route' => 'admin.whitelabels.content',
                 'url'   => route('admin.whitelabels.content')
             ],
