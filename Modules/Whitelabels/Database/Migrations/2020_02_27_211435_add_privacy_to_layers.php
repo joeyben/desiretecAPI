be933b65c68a94d1b519253a88e1f8d6edf147f2
Goldoni <fotsa.goldoni@yahoo.fr>
2020-02-27 21:17:50 +0100

    add migration for privacy link

---
+++ Modules/Whitelabels/Database/Migrations/2020_02_27_211435_add_privacy_to_layers.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddPrivacyToLayers extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('layer_whitelabel', function (Blueprint $table) {
+            if (Schema::hasTable('layer_whitelabel') && !Schema::hasColumn('layer_whitelabel', 'privacy')) {
+                Schema::table('layer_whitelabel', function (Blueprint $table) {
+                    $table->string('privacy')->nullable()->after('subheadline_success');
+                });
+            }
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('', function (Blueprint $table) {
+
+        });
+    }
+}
