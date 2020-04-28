41d0c27be23cb62c69901211366fbf874d651cea
Goldoni <fotsa.goldoni@yahoo.fr>
2020-04-28 10:12:33 +0200

    remove sub title

--- Modules/Dashboard/Resources/assets/js/modules/provider/dashboard/components/ChartWishDayComponent.vue
+++ Modules/Dashboard/Resources/assets/js/modules/provider/dashboard/components/ChartWishDayComponent.vue
@@ -32,9 +32,6 @@
           title: {
             text: this.trans('dashboard.daily_average_wish')
           },
-          subtitle: {
-            text: this.trans('dashboard.current_month')
-          },
           xAxis: {
             type: 'datetime',
             dateTimeLabelFormats: { // don't display the dummy year
